1643a8ca9902d5c8652b35405758b7b229d10b57
Ilio Catallo <ilio.catallo@gmail.com>
2026-04-19 09:40:07 +0200

    Add test “Pokedex responds with not found when the Pokemon does not exist”

--- test/Pokedex.test.ts
+++ test/Pokedex.test.ts
@@ -11,3 +11,13 @@ Deno.test("Pokedex responds with Pokemon", async () => {
 
   assertEquals(await res.json(), { name: "mewtwo" });
 });
+
+Deno.test("Pokedex responds with not found when the Pokemon does not exist", async () => {
+  await using pokedex = new Pokedex(PORT);
+  await pokedex.ready;
+
+  const res = await fetch("http://localhost:" + PORT + "/pokemon/rony");
+
+  assertEquals(res.status, 404);
+  await res.body?.cancel();
+});
