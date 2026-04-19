c65cb2f889da047db98d2e3ddd98ce0460ddb1b1
Ilio Catallo <ilio.catallo@gmail.com>
2026-04-19 19:10:20 +0200

    Retry the request against PokeAPI in case of a network glitch

--- app/PokeApi.ts
+++ app/PokeApi.ts
@@ -1,20 +1,38 @@
-import { Type } from "supposedly";
-import { isValid } from "supposedly";
+import { retry } from "@std/async";
+import { isValid, Type } from "supposedly";
 import { PokeApiLike } from "@app/PokemonIndexViaPokeApi.ts";
 
 export class PokeApi implements PokeApiLike {
   async getSpecies<T>(species: string, type: Type<T>) {
-    const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon-species/${species}`,
-    );
-    if (!response.ok) {
-      await response.body?.cancel();
+    try {
+      return await retry(async () => {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon-species/${species}`,
+        );
+        if (response.status === 404) return undefined;
+        if (!response.ok) {
+          await response.body?.cancel();
+          throw new TransientHttpError(response.status);
+        }
+        const json = await response.json();
+        if (!isValid(type, json)) {
+          return undefined;
+        }
+        return json;
+      }, {
+        isRetriable: (err) =>
+          err instanceof TransientHttpError && err.status >= 500,
+      });
+    } catch {
       return undefined;
     }
-    const json = await response.json();
-    if (!isValid(type, json)) {
-      return undefined;
-    }
-    return json;
+  }
+}
+
+class TransientHttpError extends Error {
+  public status: number;
+  constructor(status: number) {
+    super(`HTTP ${status}`);
+    this.status = status;
   }
 }
